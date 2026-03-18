610d9f345277073d94e6deb8960bb0b1a6d20b47
Hyun Lee <wildriver1105@gmail.com>
2026-03-18 21:40:04 +0900

    Initial Commit

---
+++ C++ Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++ Project
+//
+//  Created by Hyun Lee on 3/18/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
